8fe92fb683d39b5a4b601ae49c65d38d13ab6263
nullMethod <null.method@gmail.com>
2018-02-01 22:08:20 +0000

    Fix bug in config that stopped the profile pic from taking the user to the root page

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,20 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Stu  Hilton'
-SITENAME = u'Stu Hilton the Software Engineer'
-SITEURL = ''
-SITETITLE = 'Stu Hilton'
+AUTHOR = 'Stu  Hilton'
+SITEURL = 'http://localhost:8000'
+SITENAME = 'Stu Hilton the Software Engineer'
+SITETITLE = AUTHOR
 SITESUBTITLE = 'Software Engineer'
-SITEDESCRIPTION = 'description description'
+SITEDESCRIPTION = '%s\'s thoughts' % AUTHOR
 SITELOGO = SITEURL + '/images/profile.png'
 FAVICON = SITEURL + '/images/favicon.ico'
 
 THEME = "/home/stu/pelican-themes/Flex"
-
 PATH = 'content'
 TIMEZONE = 'Europe/London'
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
 SOCIAL = (('linkedin', 'https://www.linkedin.com/in/stuhilton'),
           ('github', 'https://github.com/nullMethod'),
